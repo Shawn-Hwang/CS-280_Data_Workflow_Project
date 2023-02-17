4414a41c8b6cbe8dce6705aed057118648ca7df4
Shawn <seanzy0716@gmail.com>
2023-02-17 16:48:10 -0700

    Hard coded db info

--- models/config.py
+++ models/config.py
@@ -3,11 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 def create_dburl():
-    hostname = Variable.get("DATABASE_USERNAME")
-    username = Variable.get("DATABASE_PASSWORD")
-    password = Variable.get("DATABASE_HOSTNAME")
-    port = Variable.get("DATABASE_PORT")
-    database = Variable.get("DATABASE_NAME")
+    # hostname = Variable.get("DATABASE_USERNAME")
+    # username = Variable.get("DATABASE_PASSWORD")
+    # password = Variable.get("DATABASE_HOSTNAME")
+    # port = Variable.get("DATABASE_PORT")
+    # database = Variable.get("DATABASE_NAME")
+    hostname = '0.0.0.0'
+    username = 'postgres'
+    password = 12345
+    port = 5432
+    database = 'twitter'
     return f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
 
 engine = create_engine(create_dburl())
