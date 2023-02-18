2bc00fa21450bf7266c6db59f9474b25f6f7a6e8
Shawn <seanzy0716@gmail.com>
2023-02-17 18:38:50 -0700

    Tried to insert user

--- models/users.py
+++ models/users.py
@@ -7,9 +7,9 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, not_null=True)
     user_id = Column(Integer, not_null=True)
-    username = Column(String, not_null=True)
-    name = Column(String, not_null=True)
-    created_at = Column(Date, not_null=True)
+    username = Column(String)
+    name = Column(String)
+    created_at = Column(Date)
     
     def __repr__(self) -> str:
         return f"User(id={self.id}, user_id={self.user_id}, username={self.username}, name={self.name}, created_at={self.created_at})" 
