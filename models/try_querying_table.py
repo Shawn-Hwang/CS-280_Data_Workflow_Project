724dab3d580a8ceb11242e00ed8f417eb5236817
Shawn <seanzy0716@gmail.com>
2023-03-02 19:34:22 -0700

    try query

---
+++ models/try_querying_table.py
@@ -0,0 +1,22 @@
+from config import Session #You would import this from your config file
+from users import User
+from datetime import datetime
+
+session = Session()
+
+# This will retrieve all of the users from the database 
+# (It'll be a list, so you may have 100 users or 0 users)
+users_lst = session.query(User).all() 
+print(users_lst)
+
+# This will retrieve the user who's username is NASA
+nasaUser = session.query(User).filter(User.username == "POTUS").first()
+
+#You can then print the username of the user you retrieved
+print(nasaUser.name)
+
+#We recommend that you reassign the user to a variable so that you can use it later
+nasaUsername = nasaUser.username
+
+# This will close the session that you opened at the beginning of the file.
+session.close()
